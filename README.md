8fc7acbb1f18e78c5082251a1fe3fc9353566bc8
Anthony Norton <anthony@modularity.io>
2018-04-27 11:45:28 +0100

    Randomised the allergy symbols.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.1.
 
 
-### Assignments
+### Coding Challenge
 1) Interpret the example JSON feed below into a responsive layout: Feed URL: https://dev.menu.ninja/api/menu/156?key=8j5vfe%24*pfb**rzt&pretty=1
 
 2) Build or outline a form to submit name, company, email address and an enquiry with appropriate validation, submitting in-page with response message based on failure/success.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ export class AppComponent implements OnInit {
   feed: string = 'https://dev.menu.ninja/api/menu/156?key=8j5vfe%24*pfb**rzt&pretty=1';
   menu: any[] = [];
   constructor(public http: Http) {
-
   }
 
   ngOnInit() {
@@ -28,7 +27,9 @@ export class AppComponent implements OnInit {
   consumeMenuItemFeed() {
     this.http.get(this.feed)
       .subscribe(response => {
+        // get response as json oblect
         const feedData = response.json();
+        // convert object to array
         this.menu = Array.from(feedData.menu.items, x => {
           return x;
         });
@@ -36,15 +37,25 @@ export class AppComponent implements OnInit {
       });
   }
 
+
+  /**
+   * Adds some mock data to the feed
+   *
+   * @memberof AppComponent
+   */
   mockData() {
     this.menu.forEach((item) => {
       // creates an array of five random 1's and 0's sorts in reverse ord to hold mock fav scores
-      item.favs = Array.from({ length: 5 }, () => Math.floor(Math.random() * 2)).sort().reverse();
-      item.favs = Array.from({ length: 5 }, () => Math.floor(Math.random() * 2)).sort().reverse();
+      item.favs = Array.from({ length: 6 }, () => Math.floor(Math.random() * 2)).sort().reverse();
       // mock random prices
       item.prices.price.price = (Math.random() * (1.120) + 2.400).toFixed(2);
+      // mock number of allergies
+      item.allergies = Array.from({ length: 4 }, () => Math.floor(Math.random() * 2));
+      // check for an array of all zeros or one
+      const sum = item.allergies.reduce((sum, x) => sum + x);
+      // if true add default alues
+      if (sum === 0 || sum === 1) { item.allergies = [0, 1, 1, 1]; }
     });
-
   }
 
 
